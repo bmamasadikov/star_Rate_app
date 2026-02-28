b0f0d07ec462b8b40312cd3a26b3c72ed0d16141
Bekhroz <bekhroz@proton.me>
2026-02-28 21:05:51 +0500

    Fix compare table refresh on accommodation type change

--- js/app.js
+++ js/app.js
@@ -1328,6 +1328,7 @@ function initEventListeners() {
             selectedAccommodationType = accommodationTypeSelect.value;
             initStarCards();
             renderClassificationCriteria();
+            renderCompareTable();
             updateStats();
         });
     }
